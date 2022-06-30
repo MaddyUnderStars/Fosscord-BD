23e171116dd9092e43fc6568fbaf50481ac80f61
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-06-30 13:44:55 +1000

    Null prototype check in findIds

--- src/util/Snowflake.ts
+++ src/util/Snowflake.ts
@@ -6,6 +6,10 @@ export const findIds = (obj: any, depth = 5): string[] => {
 		if (value && typeof value === "object")
 			ret = ret.concat(findIds(value, depth - 1));
 
+		if (value && !(value as any).toString) {
+			// prevent objects will null prototype from throwing when we convert to string later
+			continue;
+		}
 		// bad way of checking if it looks like a proper snowflake
 		if (!value || value == "null" || value == "undefined") continue;
 		if (!Number(value)) continue;
@@ -14,9 +18,9 @@ export const findIds = (obj: any, depth = 5): string[] => {
 		if (index == -1) continue;
 		if (index != key.length - 2) continue;
 		// if (key.toLowerCase().indexOf("_id") == -1	// bad detection algo lets gooo
-			// && key.toLowerCase().indexOf("id") != key.length - 2
-			// && key.length - 2 != -1)
-			// continue;
+		// && key.toLowerCase().indexOf("id") != key.length - 2
+		// && key.length - 2 != -1)
+		// continue;
 
 		// console.log(`Fosscord controls ID ${key}:${value}`);
 		ret.push(value as string);
